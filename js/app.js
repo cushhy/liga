a50366ddd2f49e238e28e55ca15571e1a98a6083
xanima <92389364+cushhy@users.noreply.github.com>
2023-01-14 09:09:54 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 $(document).ready(function () {
 
     $.get("https://ipinfo.io?token=31a360b818bfec", function (response) {
-        if (response.city == 'Kirov') {
+        if (response.region == 'Kirov Oblast') {
             $('.header__phone span').text('+7 8332 49-98-99');
             $(".header__phone").attr("href", "tel:+78332499899")
             $(".header__geo-city").text("Киров")
-        } else if (response.city == 'Syktyvkar') {
+        } else if (response.region == 'Komi Republic') {
             $('.header__phone span').text('+7 8212 46-75-75');
             $(".header__phone").attr("href", "tel:+78212467575")
             $(".header__geo-city").text("Сыктывкар")
-        } else if (response.city == 'Izhevsk') {
+        } else if (response.region == 'Udmurtia') {
             $('.header__phone span').text('+7 3412 77-14-47');
             $(".header__phone").attr("href", "tel:+73412771447")
             $(".header__geo-city").text("Ижевск")
-        } else if (response.city == 'Yekaterinburg') {
+        } else if (response.region == 'Sverdlovsk oblast') {
             $('.header__phone span').text('+7 343 361-01-04');
             $(".header__phone").attr("href", "tel:+73433610104")
             $(".header__geo-city").text("Екатеринбург")
-        } else if (response.city == 'Perm') {
+        } else if (response.region == 'Perm Krai') {
             $('.header__phone span').text('+7 342 204-05-52');
             $(".header__phone").attr("href", "tel:+73422040552")
             $(".header__geo-city").text("Пермь")
-        } else if (response.city == 'Nizhny Novgorod') {
+        } else if (response.region == 'Nizhny Novgorod Oblast') {
             $('.header__phone span').text('+7 831 423-04-14');
             $(".header__phone").attr("href", "tel:+78314230414")
             $(".header__geo-city").text("Нижний Новгород")
