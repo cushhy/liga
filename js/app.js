33b8c848602c52e7cb02ef419651efef18d14754
xanima <92389364+cushhy@users.noreply.github.com>
2023-01-14 07:57:33 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,44 +1,27 @@
 $(document).ready(function () {
-    if (YMaps.location.city != '') {
-        var youCity = (YMaps.location.city);
-        if (youCity === "Киров") {
-            $('.header__phone span').text('+7 833 249-98-99');
-            $(".header__phone").attr("href", "tel:+78332499899")
-            $(".header__geo-city").text("Киров")
+    ymaps.ready(function () {
+        var geolocation = ymaps.geolocation;
+        if (geolocation.city == undefined) {
+            // Если город не определился, то выводим "неопределен"
+            // По умолчанию город Москва (указываем в html)
+            $('.header__geo-city').html('неопределен');
+        } else {
+            $('.header__geo-city').html('' + geolocation.city);
         }
-        else if (youCity === "Сыктывкар") {
-            $('.header__phone span').text('+7 821 246-75-75');
-            $(".header__phone").attr("href", "tel:+78212467575")
-            $(".header__geo-city").text("Сыктывкар")
-        }
-        else if (youCity === "Ижевск") {
-            $('.header__phone span').text('+7 341 277-14-47');
-            $(".header__phone").attr("href", "tel:+73412771447")
-            $(".header__geo-city").text("Ижевск")
-        }
-        else if (youCity === "Екатеринбург") {
-            $('.header__phone span').text('+7 343 361-01-04');
-            $(".header__phone").attr("href", "tel:+73433610104")
-            $(".header__geo-city").text("Екатеринбург")
-        }
-        else if (youCity === "Пермь") {
-            $('.header__phone span').text('+7 342 204-05-52');
-            $(".header__phone").attr("href", "tel:+73422040552")
-            $(".header__geo-city").text("Пермь")
-        }
-        else if (youCity === "Нижний Новгород") {
-            $('.header__phone span').text('+7 831 423-04-14');
-            $(".header__phone").attr("href", "tel:+78314230414")
-            $(".header__geo-city").text("Нижний Новгород")
-        }
-        else {
-            $('.header__phone span').text('+7 999 100-80-00');
-            $(".header__phone").attr("href", "tel:+79991008000")
-            $(".header__geo-city").text("Москва")
-        }
-    };
+        // Для нужных городов прописываем свои телефоны
+        // Для других городов покажется номер по умолчанию - "+7 (123) 000-00-00" (указываем в html)
+        if (geolocation.city == "Киров") {
+            $('.header__phone span').html('+7 833 249-98-99')
+            $('.header__phone').attr('href', 'tel:+78332499899')
+        };
+        if (geolocation.city == "Екатеринбург") {
+            $('.header__phone span').html('+7 343 361-01-04')
+            $('.header__phone').attr('href', 'tel:+73433610104')
+        };
+        if (geolocation.city == "Сыктывкар") {
+            $('.header__phone span').html('+7 821 246-75-75')
+            $('.header__phone').attr('href', 'tel:+782124675754')
+        };
 
-    $('.header__burger').on('click', function () {
-        location.href = "https://liga.group";
     });
 });
